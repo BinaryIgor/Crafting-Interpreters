03e498f2471e20d178c1b8d3080330ecd34d7723
Igor <41533396+BinaryIgor@users.noreply.github.com>
2024-12-13 19:23:28 +0100

    C Linked List

--- experiments/linked_list.c
+++ experiments/linked_list.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
 
 int main()
 {
-    char message[] = "Hello, world!";
-    printf(message);
+    Node *first = (Node *)malloc(sizeof(Node));
+
+    first->data = 10;
+
+    Node *second = (Node *)malloc(sizeof(Node));
+
+    second->data = 20;
+
+    Node *third = (Node *)malloc(sizeof(Node));
+
+    third->data = 30;
+
+    first->next = second;
+    second->next = third;
+    third->next = NULL;
+
+    printf("Linked List: \n");
+    Node *temp = first;
+    while (temp)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+
+    free(first);
+    free(second);
+    free(third);
     return 0;
 }
